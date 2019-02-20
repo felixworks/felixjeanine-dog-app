3454ca174dc50428b67179afcec447fc3a9e9ad4
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-02-19 21:46:10 -0800

    ex2

--- scripts.js
+++ scripts.js
@@ -1,43 +1,61 @@
 // Assignment 1
 
+// function getDogs(userInput) {
+//     let request = `https://dog.ceo/api/breeds/image/random/${userInput}`
+//     fetch(request)
+//         .then(response => response.json())
+//         .then(responseJson => {
+//             console.log(responseJson);
+//             return responseJson;})
+//         .catch(error => alert('Something went wrong. Try again later.'));
+// }
+
+
+
+// function buttonHandler() {
+//     $('form').submit(event => {
+//         event.preventDefault();
+//         getDogs($('#dogInput').val());
+//     });
+// }
+
+// $(buttonHandler());
+
+
+// ex. 2
 function getDogs(userInput) {
     let request = `https://dog.ceo/api/breeds/image/random/${userInput}`
     fetch(request)
         .then(response => response.json())
         .then(responseJson => {
             console.log(responseJson);
-            return responseJson;})
-        .catch(error => alert('Something went wrong. Try again later.'));
-}
+            displayResults(responseJson);
+        })
+        .catch(error => alert('Something went wrong. Try again later.')); 
+    }
+    
+    function displayResults(responseJson) {
+        console.log(responseJson);
+        //replace the existing image with the new one
+        let htmlString = '';
+        for(let i=0; i<= responseJson.message.length; i++){
+            let temp=`<img src="${responseJson.message[i]}" class="results-div"><br>`
+            htmlString+=temp
+        }
+        console.log('htmlString', htmlString);
+        $('.results-div').html(htmlString)
+            //display the results section
+            // $('.results-div');
+        }
+        
+        function buttonHandler() {
+            $('form').submit(event => {
+                event.preventDefault();
+                // let dogPictureDisplay = $('#dogInput').val();
+                // console.log(dogPictureDisplay);
+                getDogs($('#dogInput').val());
+            });
+        }
+    
+    $(buttonHandler());
 
-
-
-function buttonHandler() {
-    $('form').submit(event => {
-        event.preventDefault();
-        getDogs($('#dogInput').val());
-    });
-}
-
-$(buttonHandler());
-
-// function getDogs() {
-//     fetch('https://dog.ceo/api/breeds/image/random/3')
-//         .then(response => response.json())
-//         .then(responseJson => {
-//             console.log(responseJson);
-//             return responseJson;})
-//         .then(responseJson => 
-//             displayResults(responseJson))
-//         .catch(error => alert('Something went wrong. Try again later.'));
-// }
-
-// function displayResults(responseJson) {
-//     console.log(responseJson);
-//     //replace the existing image with the new one
-//     $('.results-div').replaceWith(
-//       `<img src="${responseJson.message}" class="results-img">`
-//     )
-//     //display the results section
-//     $('.results').removeClass('hidden');
-// }
